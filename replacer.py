38d114d065109bdee6b2fffad9050f34e2485bc5
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-04-02 18:29:30 +0300

    upd

--- replacer.py
+++ replacer.py
@@ -12,16 +12,16 @@ lover_nospace_site_name = nospace_site_name.lower()
 upper_nospace_site_name = nospace_site_name.upper()
 
 # where to get data from
-wg_terms = Path.cwd()/('wisegazette terms.txt')
-wg_about_us = Path.cwd()/('wisegazette about us.txt')
-wg_contact_us = Path.cwd()/('wisegazette contact us.txt')
-wg_privacy_policy = Path.cwd()/('wisegazette privacy policy.txt')
+wg_terms = Path.cwd()/('wg terms.txt')
+wg_about_us = Path.cwd()/('wg about.txt')
+wg_contact_us = Path.cwd()/('wg contact.txt')
+wg_privacy_policy = Path.cwd()/('wg privacy.txt')
 
 # where to put modified data
 new_terms = Path.cwd()/('new terms.txt')
-new_about_us = Path.cwd()/('new about us.txt')
-new_contact_us = Path.cwd()/('new contact us.txt')
-new_privacy_policy = Path.cwd()/('new privacy policy.txt')
+new_about_us = Path.cwd()/('new about.txt')
+new_contact_us = Path.cwd()/('new contact.txt')
+new_privacy_policy = Path.cwd()/('new privacy.txt')
 
 # opening files
 wg_terms_file = open(wg_terms, 'r')
@@ -58,7 +58,7 @@ wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site
 
 file = open(new_terms, 'w')
 file.write(fileContent)
-print('done')
+print('done')#
 
 
 
