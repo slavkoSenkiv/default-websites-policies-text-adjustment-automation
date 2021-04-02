174c0d3fa03c160d2fd8034743de28530f4eea2e
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-04-02 22:53:14 +0300

    upd

--- replacer.py
+++ replacer.py
@@ -1,64 +1,38 @@
-# wg wisegazette
-# new
-
 from pathlib import Path
 
-site_name = input('enter website name: \n(example: Just A Conservative)\n')
+site_name = input('enter website name (example: Just A Conservative): ')  # 1 Just A Conservative
 
-lover_site_name = site_name.lower()
-upper_site_name = site_name.upper()
-nospace_site_name = site_name.replace(' ', '')
-lover_nospace_site_name = nospace_site_name.lower()
-upper_nospace_site_name = nospace_site_name.upper()
-
-# where to get data from
-wg_terms = Path.cwd()/('wg terms.txt')
-wg_about_us = Path.cwd()/('wg about.txt')
-wg_contact_us = Path.cwd()/('wg contact.txt')
-wg_privacy_policy = Path.cwd()/('wg privacy.txt')
-
-# where to put modified data
-new_terms = Path.cwd()/('new terms.txt')
-new_about_us = Path.cwd()/('new about.txt')
-new_contact_us = Path.cwd()/('new contact.txt')
-new_privacy_policy = Path.cwd()/('new privacy.txt')
-
-# opening files
-wg_terms_file = open(wg_terms, 'r')
-wg_terms_file_content = wg_terms_file.read()
-
-wg_about_us_file = open(wg_about_us, 'r')
-wg_about_us_file_content = wg_about_us_file.read()
-
-wg_contact_us_file = open(wg_contact_us, 'r')
-wg_contact_us_file_content = wg_contact_us_file.read()
-
-wg_privacy_policy_file = open(wg_privacy_policy, 'r')
-wg_privacy_policy_file_content = wg_privacy_policy_file.read()
-
-wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
-wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
-wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
-wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site_name)
-
-wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
-wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
-wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
-wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site_name)
-
-wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
-wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
-wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
-wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site_name)
-
-wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
-wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
-wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
-wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site_name)
-
-file = open(new_terms, 'w')
-file.write(fileContent)
-print('done')#
+# here we convert website name to all needed formats
+lover_site_name = site_name.lower()                     # 2 just a conservative
+upper_site_name = site_name.upper()                     # 3 JUST A CONSERVATIVE
+nospace_site_name = site_name.replace(' ', '')          # 4 JustAConservative
+lover_nospace_site_name = nospace_site_name.lower()     # 5 justaconservative
+upper_nospace_site_name = nospace_site_name.upper()     # 6 JUSTACONSERVATIVE
 
 
+def in_page_replacer(page):
+
+    # privacy doc section
+    wg_file_path = Path.cwd()/(f'wg {page}.txt')
+    new_file_path = Path.cwd()/(f'new {page}.txt')
+
+    wg_file = open(wg_file_path, 'r')
+    wg_file_content = wg_file.read()
+
+    wg_file_content = wg_file_content.replace('Wise Gazette', site_name)
+    wg_file_content = wg_file_content.replace('wisegazette', lover_nospace_site_name)
+    wg_file_content = wg_file_content.replace('Wisegazette', lover_nospace_site_name)
+    wg_file_content = wg_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
+    wg_file_content = wg_file_content.replace('WISE GAZETTE', upper_site_name)
+
+    new_file = open(new_file_path, 'w')
+    new_file.write(f'{page}\n')
+    new_file.write(wg_file_content)
+
+in_page_replacer('Contact Us')
+in_page_replacer('About Us')
+in_page_replacer('Privacy Policy')
+in_page_replacer('Terms Of Conditions')
+
+print('\ndone')
 
