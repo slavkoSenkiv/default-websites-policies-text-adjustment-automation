5a7e5a69e431d0e57629d95cb0165ae40e00d1f4
slavkoSenkiv <slavko.senkiv@gmail.com>
2021-04-03 13:14:49 +0300

    upd

--- replacer.py
+++ replacer.py
@@ -27,6 +27,7 @@ new_privacy_policy = Path.cwd()/('new privacy.txt')
 wg_terms_file = open(wg_terms, 'r')
 wg_terms_file_content = wg_terms_file.read()
 
+<<<<<<< Updated upstream
 wg_about_us_file = open(wg_about_us, 'r')
 wg_about_us_file_content = wg_about_us_file.read()
 
@@ -40,6 +41,13 @@ wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
 wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
 wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
 wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site_name)
+=======
+def in_page_replacer(page):
+
+    wg_file_path = Path.cwd()/(f'wg {page}.txt')
+    wg_file = open(wg_file_path, 'r')
+    wg_file_content = wg_file.read()
+>>>>>>> Stashed changes
 
 wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
 wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
@@ -51,6 +59,7 @@ wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospa
 wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
 wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site_name)
 
+<<<<<<< Updated upstream
 wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
 wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
 wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
@@ -62,3 +71,16 @@ print('done')#
 
 
 
+=======
+while True:
+    doc = pyinputplus.inputMenu(['Contact Us',
+                                 'About Us Health',
+                                 'About Us Non Health',
+                                 'Privacy Policy',
+                                 'Terms And Conditions',
+                                 'Footer'],
+                                 'Pick which doc you want copy to clipboard:\n', numbered=True)
+    in_page_replacer(doc)
+    print(f'\n{doc} modified text copied to clipboard\n')
+
+>>>>>>> Stashed changes
