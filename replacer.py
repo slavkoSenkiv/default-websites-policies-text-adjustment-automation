cf19859888d2628b585fbfe42ca104827497e7c0
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-07-22 18:22:59 +0300

    Update replacer.py

--- replacer.py
+++ replacer.py
@@ -11,12 +11,10 @@ nospace_site_name = site_name.replace(' ', '')          # 4 JustAConservative
 lover_nospace_site_name = nospace_site_name.lower()     # 5 justaconservative
 upper_nospace_site_name = nospace_site_name.upper()     # 6 JUSTACONSERVATIVE
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
+
 wg_about_us_file = open(wg_about_us, 'r')
 wg_about_us_file_content = wg_about_us_file.read()
-=======
->>>>>>> 56d86a9866204cf39f7145dd95d5d5742ea9ecda
+
 
 def in_page_replacer(page):
 
@@ -24,22 +22,21 @@ def in_page_replacer(page):
     wg_file_path = Path.cwd()/(f'wg {page}.txt')
     new_file_path = Path.cwd()/(f'new {page}.txt')
 
-<<<<<<< HEAD
+
 wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
 wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
 wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
 wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site_name)
-=======
+
 def in_page_replacer(page):
 
     wg_file_path = Path.cwd()/(f'wg {page}.txt')
     wg_file = open(wg_file_path, 'r')
     wg_file_content = wg_file.read()
->>>>>>> Stashed changes
-=======
+
     wg_file = open(wg_file_path, 'r')
     wg_file_content = wg_file.read()
->>>>>>> 56d86a9866204cf39f7145dd95d5d5742ea9ecda
+
 
     wg_file_content = wg_file_content.replace('Wise Gazette', site_name)
     wg_file_content = wg_file_content.replace('wisegazette', lover_nospace_site_name)
@@ -51,17 +48,15 @@ def in_page_replacer(page):
     modified_value = f'{page}\n' + wg_file_content
     pyperclip.copy(modified_value)
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
+
 wg_terms_file_content = wg_terms_file_content.replace('Wise Gazette', site_name)
 wg_terms_file_content = wg_terms_file_content.replace('wisegazette', lover_nospace_site_name)
 wg_terms_file_content = wg_terms_file_content.replace('WISEGAZETTE', upper_nospace_site_name)
 wg_terms_file_content = wg_terms_file_content.replace('WISE GAZETTE', upper_site_name)
-=======
+
 """    new_file = open(new_file_path, 'w')
     new_file.write(f'{page}\n')
     new_file.write(wg_file_content)"""
->>>>>>> 56d86a9866204cf39f7145dd95d5d5742ea9ecda
 
 while True:
     doc = pyinputplus.inputMenu(['Contact Us', 'About Us Health', 'About Us Non Health', 'Privacy Policy', 'Terms And Conditions', 'Footer'], 'Pick which doc u want copy to clipboard:\n', numbered=True)
@@ -76,7 +71,7 @@ in_page_replacer('Footer')"""
 
 print('\ndone')
 
-=======
+
 while True:
     doc = pyinputplus.inputMenu(['Contact Us',
                                  'About Us Health',
@@ -88,4 +83,3 @@ while True:
     in_page_replacer(doc)
     print(f'\n{doc} modified text copied to clipboard\n')
 
->>>>>>> Stashed changes
